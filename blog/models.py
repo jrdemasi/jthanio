7078ade87556b727f4ff1183b0a2ea240f0ad9f4
Jonathan DeMasi <jrdemasi@gmail.com>
2020-03-02 21:40:33 -0700

    category support backend. Tags fronend

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ from wagtail.search import index
 from modelcluster.fields import ParentalKey
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
+from wagtail.snippets.models import register_snippet
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
@@ -49,3 +50,30 @@ class BlogPost(Page):
         FieldPanel('intro'),
         FieldPanel('body'),
     ]
+
+class BlogTagIndexPage(Page):
+
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPost.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['blogpages'] = blogpages
+        return context
+        
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+    panels = [
+        FieldPanel('name')
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'blog categories'
