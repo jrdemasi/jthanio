fb0f126edc01987143b3047d14c096647150a36e
Jonathan DeMasi <jonathan.demasi@colorado.edu>
2020-04-02 21:37:19 -0600

    okay, final changes for settings for prod I think

--- jthanio/settings/production.py
+++ jthanio/settings/production.py
@@ -6,3 +6,13 @@ try:
     from .local import *
 except ImportError:
     pass
+
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'OPTIONS': {
+            'read_default_file': os.path.join(os.path.expanduser('~'), '.my.cnf'),
+         }
+     }
+ }
+
